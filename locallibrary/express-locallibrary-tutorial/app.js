f168cecd4d2c9bff63752b29b75f3f832e1c3007
Hojin Ryu <robert.ryu01@gmail.com>
2023-04-12 22:09:18 -0300

    mongoose setup

--- locallibrary/express-locallibrary-tutorial/app.js
+++ locallibrary/express-locallibrary-tutorial/app.js
@@ -9,6 +9,21 @@ const usersRouter = require('./routes/users');
 
 const app = express();
 
+// mongoose setup 
+// Import the mongoose module
+const mongoose = require("mongoose");
+
+mongoose.set('strictQuery', false);
+
+// Define the database URL to connect to.
+const mongoDB = "mongodb://127.0.0.1/my_database";
+
+// Wait for database to connect, logging an error if there is a problem 
+main().catch(err => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
